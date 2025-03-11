933e9ff85fa26fd7d1492e1ccc87d82f033f8d2e
smspocoredondo <smspocoredondo@gmail.com>
2025-03-11 07:10:25 -0300

    Update dashboard_atendimentos.py

--- dashboard_atendimentos.py
+++ dashboard_atendimentos.py
@@ -41,7 +41,7 @@ if uploaded_file:
     if 'Especialidade' in df_clean.columns:
         especialidades = df_clean['Especialidade'].value_counts().reset_index()
         especialidades.columns = ['Especialidade', 'Quantidade']
-        fig1 = px.pie(especialidades, names='Especialidade', values='Quantidade', title='% de Atendimenos por Categoria')
+        fig1 = px.pie(especialidades, names='Especialidade', values='Quantidade', title='% de Atendimentos por Categoria')
         col1.plotly_chart(fig1)
 
     # Seleção e contagem por categoria de profissional
